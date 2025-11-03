c8f266c1fdc1cbb95399b5c180155797ff27fd9d
Tinotenda Mawaro <tmawaro25@gmail.com>
2025-11-03 15:22:11 +0000

    The logout button isnot working

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppHeader } from "@/components/app/app-header";
 import AppSidebar from "@/components/app/app-sidebar";
+import { AuthGuard } from "@/hooks/use-auth";
 
 export default function DashboardLayout({
   children,
@@ -8,12 +9,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-        <AppSidebar />
-        <div className="flex-1 flex flex-col min-h-screen">
-          <AppHeader />
-          <main className="flex-1 bg-muted/40 p-4 md:p-6">{children}</main>
-        </div>
-    </SidebarProvider>
+    <AuthGuard>
+      <SidebarProvider>
+          <AppSidebar />
+          <div className="flex-1 flex flex-col min-h-screen">
+            <AppHeader />
+            <main className="flex-1 bg-muted/40 p-4 md:p-6">{children}</main>
+          </div>
+      </SidebarProvider>
+    </AuthGuard>
   );
 }
